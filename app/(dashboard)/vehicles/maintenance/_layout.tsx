677b5afe2496430faa78d221ed7e8680b0d5d8e6
Shachiru <shachirurashmika35@gmail.com>
2025-09-15 09:53:25 +0530

    can add vehicle maintenance tasks.

--- app/(dashboard)/vehicles/maintenance/_layout.tsx
+++ app/(dashboard)/vehicles/maintenance/_layout.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const MaintenanceLayout = () => {
   return (
     <Stack screenOptions={{ animation: "fade_from_bottom" }}>
-      <Stack.Screen name="[vehicleId]" options={{ headerShown: false }} />
+      <Stack.Screen name="[vehicleId]/index" options={{ headerShown: false }} />
       <Stack.Screen name="task/[id]" options={{ title: "Maintenance Task" }} />
     </Stack>
   );
