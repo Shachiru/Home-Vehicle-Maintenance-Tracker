fe03e1e43bd489ae86c9887db74387c1c958739a
Shachiru <shachirurashmika35@gmail.com>
2025-09-22 10:49:14 +0530

    add new files

---
+++ types/istanbul/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for Istanbul packages
+
+declare module "istanbul-lib-report" {
+  // Empty interface to satisfy TypeScript
+}
+
+declare module "istanbul-reports" {
+  // Empty interface to satisfy TypeScript
+}
